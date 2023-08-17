b2704cddf5014b5cbc54bf5b29790901ed1b78b7
Eben <ebenezergana@gmail.com>
2023-08-17 21:52:32 +0100

    update project

--- src/main/kotlin/com/eventense/Application.kt
+++ src/main/kotlin/com/eventense/Application.kt
@@ -2,8 +2,16 @@ package com.eventense
 
 import com.eventense.plugins.*
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>):Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>) {
+    val port = System.getenv("PORT")?.toInt() ?: 8080 // Default to 8080 if PORT environment variable is not provided
+
+    embeddedServer(Netty, host = "0.0.0.0", port = port) {
+        module()
+    }.start(wait = true)
+}
 
 fun Application.module() {
     configureKoin()
